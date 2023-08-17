10fe4344dc19027c4b6bc94bef8aba30868a1569
Vitor Mattos <vitor@php.rio>
2023-08-16 22:48:26 -0300

    Replace contact form
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- source/sobre.blade.php
+++ source/sobre.blade.php
@@ -11,7 +11,7 @@ description: A little bit about the site
         alt="About image"
         class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
+    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page.</p>
 
     <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
 
