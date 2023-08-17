bcf3c3f90a1fc20c8b7eafc833b71fe5aefb66ff
Vitor Mattos <vitor@php.rio>
2023-08-16 21:00:51 -0300

    Lock the PHP version and set default environments
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set uid of host machine
+usermod --non-unique --uid "${HOST_UID}" www-data
+groupmod --non-unique --gid "${HOST_GID}" www-data
+
 # Composer
 if [ ! -d "vendor" ]; then
     composer install
